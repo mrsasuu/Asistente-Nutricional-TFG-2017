ca20a9bc37c14e983fd0d6f7d80501e51d250596
Unknown <mrsasuu@gmail.com>
2017-06-08 13:14:36 +0200

    Avance recycler view

---
+++ AN/mobile/src/main/java/com/example/sasu/asistente_nutricional_tfg_2017/adapters/AdapterRegistro.java
@@ -0,0 +1,25 @@
+package com.example.sasu.asistente_nutricional_tfg_2017.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by mrsas on 08/06/2017.
+ */
+
+public class AdapterRegistro extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
