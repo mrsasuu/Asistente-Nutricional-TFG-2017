8a714026fd918f8d458905c44f9849202c751739
mrsasuu <mrsasuu@gmail.com>
2017-07-29 18:24:49 +0200

    show patient con calendario y datos personales + estadiscias
    Faltan por completar las estadisitcas, el calendario y algunas cosas

--- server/public/static/js/src/patient_b.js
+++ server/public/static/js/src/patient_b.js
@@ -6,6 +6,22 @@ $(document).ready(function() {
 		}
 	});
 
+	$("#close-card").on('click', function() {
+	    //$(".card-reveal").css({"transform":"translateY(0%)","display":"none"});
+            $( "#card-description" ).removeClass("modal-shown");
+            $( "#card-description" ).addClass("modal-hidden");
+
+	});
+
+    $("#open-card").on('click', function() {
+        //$(".card-reveal").css({"transform":"translateY(0%)","display":"none"});
+
+
+
+            $( "#card-description" ).removeClass("modal-hidden");
+            $( "#card-description" ).addClass("modal-shown");
+    });
+
 	// If click on send but delete yes check is not checked close the modal
 	$('#delete_patient form').submit(function(event) {
 		if(document.getElementById("delete_patient_yes").checked)
@@ -82,7 +98,11 @@ $(document).ready(function() {
                     $("#edit_height_patient").val(jsondata.HEIGHT);
                     $("#edit_weight_patient").val(jsondata.WEIGHT);
                     $("#editPatientImgViewer").attr('src', jsondata.PHOTO);
-                    $("#edit_previous_photo_user").val(jsondata.PHOTO);
+
+                    if(jsondata.PHOTO != null)
+                        $("#editPatientImgViewer").attr('src', jsondata.PHOTO);
+                    else
+                        $("#editPatientImgViewer").attr('src', "/static/img/img_not_available.png");
 
 
 
@@ -97,6 +117,7 @@ $(document).ready(function() {
 
 
 		}else if(action == '#show_patient') {
+
             $.ajax({
                 type: "GET",
                 url: "/api/patient/id/"+id_patient,
@@ -108,11 +129,7 @@ $(document).ready(function() {
                     $("#show_patient_male").removeAttr('checked');
                     $("#show_patient_female").removeAttr('checked');
 
-                    if(gender){
-                        $("#show_patient_female").prop('checked', true);
-                    }else{
-                        $("#show_patient_male").prop('checked', true);
-                    }
+
 
                     var activity = parseInt(jsondata.ACTIVITY_LEVEL);
                     $("#show_activity_level_patient_1").removeAttr('checked');
@@ -122,20 +139,49 @@ $(document).ready(function() {
 
                     switch(activity){
                         case 0:
-                            $("#show_activity_level_patient_1").prop('checked', true);
+                            $("#show_patient_activity").text("Sedentario");
                             break;
                         case 1:
-                            $("#show_activity_level_patient_2").prop('checked', true);
+                            $("#show_patient_activity").text("Moderada");
                             break;
                         case 2:
-                            $("#show_activity_level_patient_3").prop('checked', true);
+                            $("#show_patient_activity").text("Intensa");
                             break;
                         case 3:
-                            $("#show_activity_level_patient_4").prop('checked', true);
+                            $("#show_patient_activity").text("Muy intensa");
                             break;
                     }
 
-                    $("#show_name_patient").val(jsondata.NAME);
+                    $("#show_patient_name_1").text(jsondata.NAME + " " + jsondata.SURNAME);
+                    $("#show_patient_name_2").text(jsondata.NAME + " " + jsondata.SURNAME);
+                    $("#show_patient_email").text(jsondata.EMAIL);
+
+                    $("#show_patient_dni_1").text(jsondata.DNI);
+                    $("#show_patient_dni_2").text(jsondata.DNI);
+
+                    $("#show_patient_age").text(jsondata.AGE);
+                    $("#show_patient_username").text(jsondata.USERNAME);
+
+                    $("#show_patient_height").text(jsondata.HEIGHT + " cm");
+                    $("#show_patient_weight").text(jsondata.WEIGHT + " Kg");
+
+                    if(jsondata.PHOTO != null)
+                        $("#show_patient_profile_picture").attr('src', jsondata.PHOTO);
+
+                    $("#show_patient_address").text(jsondata.ADDRESS);
+                    $("#show_patient_phone").text(jsondata.PHONE);
+
+
+                    if(gender){
+                        $("#show_patient_gender").text("Mujer");
+                    }else{
+                        $("#show_patient_gender").text("Hombre");
+                    }
+
+
+
+
+
                     $("#show_dni_patient").val(jsondata.DNI);
                     $("#show_age_patient").val(jsondata.AGE);
                     $("#show_surname_patient").val(jsondata.SURNAME);
